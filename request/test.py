6ba1ba417ee67c0f74a582d72436d3dfb1fb6980
sryan <sun.ryan@acxiom.com>
2016-06-12 11:33:26 +0800

    Merge branch 'master' of https://github.com/zzzzzsy/FY17Q1GOAL
    # Conflicts:
    #	request/httpreq.py

--- request/test.py
+++ request/test.py
@@ -1,4 +1,43 @@
+from collections import deque
+import sys
 import time
-print(time.time(), time.time()+1000)
-temp = []
-print(type(temp))
+from urllib.request import (Request, urlopen)
+
+
+def countdown(n):
+    while n > 0:
+        print('count down: ', n)
+        yield
+        n -= 1
+
+
+def countup(n):
+    x = 0
+    while x < n:
+        print('count up: ', x)
+        time.sleep(5)
+        yield
+        x += 1
+
+
+class Task:
+    def __init__(self):
+        self._task_deque = deque()
+
+    def new_task(self, task):
+        self._task_deque.append(task)
+
+    def run(self):
+        while self._task_deque:
+            task = self._task_deque.popleft()
+            try:
+                next(task)
+                self._task_deque.append(task)
+            except StopIteration:
+                sys.exit(0)
+
+
+c = countdown(1)
+print(c)
+next(c)
+next(c)
