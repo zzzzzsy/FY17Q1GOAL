835a7b5ce1e6ee30a43207ade37c0640d8b424d4
ryan sun <dieinthemoon@163.com>
2016-06-14 23:54:25 +0800

    20160601

--- request/httpreq.py
+++ request/httpreq.py
@@ -1,10 +1,9 @@
-from urllib.error import (HTTPError, URLError, ContentTooShortError)
 from configparser import ConfigParser
 from urllib.parse import urlencode
 from threading import Thread
 import time
-import socket
 import requests
+from queue import Queue
 
 
 def get_request():
@@ -33,19 +32,44 @@ def get_request():
 
 
 class Request:
-    def __init__(self, url, headers, method, para):
+    def __init__(self, url, headers=None, method='GET', para=None):
         self.url = url
-        self.headers = headers
+        if headers:
+            self.headers = headers
+        else:
+            self.headers = {}
+        if 'user-agent' not in [header.lower() for header in self.headers]:
+            self.add_header('User-Agent', 'Mozilla/4.0 (compatible; Pylot)')
+        # if 'connection' not in [header.lower() for header in self.headers]:
+        #     self.add_header('Connection', 'close')
         self.method = method
         self.para = para
 
+    def add_header(self, header, value):
+        self.headers[header] = value
+
+
+class ErrResponse:
+    def __init__(self, msg='Unknow Error Message'):
+        self.code = -1
+        self.msg = msg
+        self.headers = {}
+
 
 class LoadVU(Thread):
-    def __init__(self, reqs, thinktime=0):
+    def __init__(self, id, think_time, req_list, result_states, err_states, result_queue):
         Thread.__init__(self)
         self.running = True
-        self.reqs = reqs
-        self.thinktime = thinktime
+        self.id = id
+        self.think_time = think_time
+        self.req_list = req_list
+        self.result_states = result_states
+        self.err_states = err_states
+        self.result_queue = result_queue
+
+        self.count = 0
+        self.err_count = 0
+        self.excep_count = 0
 
     def send(self, req):
         try:
@@ -56,59 +80,114 @@ class LoadVU(Thread):
             content = resp.content
             end_time = time.clock()
         except requests.exceptions.Timeout:
-            resp = None
             conn_end_time = time.clock()
-            content = 'Time Out'
+            content = 'Time Out Error'
+            resp = ErrResponse(content)
             end_time = time.clock()
         except requests.exceptions.ConnectionError:
-            resp = None
-            time.sleep(1)
             conn_end_time = time.clock()
-            content = 'ConnectionError'
+            content = 'Connection Error'
+            resp = ErrResponse(content)
             end_time = time.clock()
+            time.sleep(1)
         return resp, content, start_time, end_time, conn_end_time
 
     def run(self):
         vu_start_time = time.clock()
+        total_bytes = 0
         while self.running:
-            for req in self.reqs:
-                time.sleep(self.thinktime)
+            for req in self.req_list:
                 if self.running:
                     resp, content, start_time, end_time, conn_end_time = self.send(req)
+                    error_flag = False
+                    excep_flag = False
+                    if resp.code >= 400:
+                        error_flag = True
+                    elif resp.code <= 0:
+                        excep_flag = True
+                    else:
+                        total_bytes += len(content)
+                    if error_flag:
+                        self.err_count += 1
+                    if excep_flag:
+                        self.excep_count += 1
+                    self.count += 1
+                    self.result_states[self.id] = ResultState(resp.code, resp.reason, self.count,
+                                                              self.err_count, self.excep_count, total_bytes)
+
                     print('res time is:' + str(end_time - start_time))
                 else:
                     break
         vu_end_time = time.clock()
+        self.result_states[self.id].vu_start_time = vu_start_time
+        self.result_states[self.id].vu_end_time = vu_end_time
         print('vu running time is:' + str(vu_end_time - vu_start_time))
 
     def stop(self):
         self.running = False
 
 
+class ResultState:
+    def __init__(self, code, reason, count, err_coount, excep_count, total_bytes):
+        self.code = code
+        self.reason = reason
+        self.count = count
+        self.err_count = err_coount
+        self.excep_count = excep_count
+        self.total_bytes = total_bytes
+        self.vu_start_time = None
+        self.vu_end_time = None
+
+
+class WorkLoad:
+    def __init__(self, ramp_up, think_time, vu_nums):
+        self.ramp_up = ramp_up
+        self.think_time = think_time
+        self.vu_nums = vu_nums
+
+
 class LoadMagr(Thread):
-    def __init__(self, reqs, num_vus, thinktime=0):
+    def __init__(self, work_load, result_states, err_states):
         Thread.__init__(self)
-        self.requests = reqs
-        self.num_vus = num_vus
+        self.work_load = work_load
+        if isinstance(work_load, WorkLoad):
+            self.num_vus = work_load.vu_nums
+            self.ranm_up = work_load.ramp_up
+            self.think_time = work_load.think_time
+        else:
+            self.num_vus = 1
+            self.ranm_up = 1
+            self.think_time = 0
         self.running = True
-        self.lstvu = []
-        self.thinktime = thinktime
-        socket.setdefaulttimeout(20)
+        self.result_states = result_states
+        self.err_states = err_states
+        self.vu_list = []
+        self.req_list = []
+        self.result_queue = Queue()
+        for i in range(self.num_vus):
+            self.result_states[i] = ResultState(0, '', 0, 0, 0, 0)
 
     def run(self):
         self.running = True
         for i in range(self.num_vus):
+            spacing = round(self.ranm_up / self.num_vus, 2)
+            if i > 0:
+                time.sleep(spacing)
             if self.running:
-                vu = LoadVU(self.requests, self.thinktime)
+                vu = LoadVU(i, self.think_time, self.req_list, self.result_states,
+                            self.err_states, self.result_queue)
                 vu.start()
                 # time.sleep(0.05)
-                self.lstvu.append(vu)
+                self.vu_list.append(vu)
                 print('VU ' + str(i) + ' started.')
 
     def stop(self):
         self.running = False
-        for vu in self.lstvu:
+        for vu in self.vu_list:
             vu.stop()
+
+    def add_req(self, req):
+        self.req_list.append(req)
 
 
 # class ErrorResponse:
@@ -116,7 +195,13 @@ class LoadMagr(Thread):
 
 
 reqs = get_request()
-t = LoadMagr(reqs, 3000)
+workload = WorkLoad(100, 0.05, 100)
+result_states = []
+err_states = []
+for i in range(200):
+    result_states.append(None)
+    err_states.append(None)
+t = LoadMagr(workload, result_states, err_states)
 t.start()
 time.sleep(50)
 t.stop()
--- request/test.py
+++ request/test.py
@@ -1,7 +1,1 @@
-import requests
 
-r = requests.get('http://en.wikipedia.org/wiki/Monty_Python')
-print(r.headers)
-print(r.request.headers)
-
-print(type({}))
