c1776a2725e5f7a1df7391c9390c3a80cc338411
sryan <sun.ryan@acxiom.com>
2016-09-13 18:00:42 +0800

    20160913

--- config/config.py
+++ config/config.py
@@ -1,17 +1,17 @@
 # General
 OUTPUT_DIR = 'D:/06_Workspace/07_Temp/'
 PROJECT_NAME = 'FY17Q1GOAL'
-REQ_LIST = '../config/requests.conf'
 REQ_XML = '../config/requests.xml'
 CONSOLE = False
 GENERATE_RESULTS = True
 REQ_TIMEOUT = 40
+ENCODING = 'UTF-8'
 
 # WorkLoad
-VUS = 500
-DURATION = 1200  # secs
-RAMPUP = 300  # secs
-INTERVAL = 0.5  # secs
+VUS = 5
+DURATION = 60  # secs
+RAMPUP = 1  # secs
+INTERVAL = 1.5  # secs
 
 # Graph
 VU_GRAPH_ENABLE = True
